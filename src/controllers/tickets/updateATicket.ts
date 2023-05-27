0087c5e390e3e0b9af36eaa894a17cc7f58fda9d
Jorge Clavijo <jym272@gmail.com>
2023-05-26 21:34:40 -0300

    refactor: update ticket

--- src/controllers/tickets/updateATicket.ts
+++ src/controllers/tickets/updateATicket.ts
@@ -12,51 +12,48 @@ const sequelize = getSequelizeClient();
 export const updateATicketController = () => {
   return async (req: Request, res: Response) => {
     const { id } = req.params;
+    const { title, price } = res.locals as TicketAttributes;
+
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- because of requireAuth middleware
     const currentUser = req.currentUser!;
     const userId = currentUser.jti;
 
-    let ticket: Ticket | null;
     try {
-      ticket = await Ticket.findByPk(id);
-    } catch (err) {
-      const error = parseSequelizeError(
-        err,
-        `Finding ticket failed. id ${id}. currentUser ${JSON.stringify(currentUser)}`
-      );
-      return throwError('Finding ticket failed.', INTERNAL_SERVER_ERROR, error);
-    }
-    if (!ticket) {
-      return throwError('Ticket not found.', NOT_FOUND);
-    }
-    if (ticket.userId !== Number(userId)) {
-      return throwError(
-        'Not authorized.',
-        UNAUTHORIZED,
-        new Error(
-          `userId in cookie payload: ${userId} is not the same as the userId found in the ticket: ${ticket.userId}`
-        )
-      );
-    }
-    if (ticket.orderId) {
-      return throwError('Ticket is reserved.', FORBIDDEN);
-    }
-    const { title, price } = res.locals as TicketAttributes;
-    let seq;
-    try {
-      await sequelize.transaction(async () => {
-        ticket?.set({ title, price: Number(price) });
-        await ticket?.save();
+      const { ticket, seq } = await sequelize.transaction(async () => {
+        const ticket = await Ticket.findByPk(id);
+        if (!ticket) {
+          return throwError('Ticket not found.', NOT_FOUND);
+        }
+        if (ticket.userId !== Number(userId)) {
+          return throwError(
+            'Not authorized.',
+            UNAUTHORIZED,
+            new Error(
+              `userId in cookie payload: ${userId} is not the same as the userId found in the ticket: ${ticket.userId}`
+            )
+          );
+        }
+        if (ticket.orderId) {
+          return throwError('Ticket is reserved.', FORBIDDEN);
+        }
+        ticket.set({ title, price: Number(price) });
+        await ticket.save();
         const pa = await publish(ticket, subjects.TicketUpdated);
-        seq = pa.seq;
+        return {
+          ticket,
+          seq: pa.seq
+        };
       });
+      return res.status(OK).json({ ticket, seq, message: 'Ticket updated.' });
     } catch (err) {
+      if (err instanceof Error) {
+        throw err;
+      }
       const error = parseSequelizeError(
         err,
         `Updating ticket failed. id ${id}. currentUser ${JSON.stringify(currentUser)}`
       );
       return throwError('Updating ticket failed.', INTERNAL_SERVER_ERROR, error);
     }
-    return res.status(OK).json({ ticket, seq, message: 'Ticket updated.' });
   };
 };
